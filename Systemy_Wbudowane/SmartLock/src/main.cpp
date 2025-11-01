74d2d76633d4d6a2f02c26620232bddf87afc4da
7ASL <arkadiusz.dojlido@gmail.com>
2025-11-01 16:14:05 +0100

    Added WiFi Functionality

--- Systemy_Wbudowane/SmartLock/src/main.cpp
+++ Systemy_Wbudowane/SmartLock/src/main.cpp
@@ -1,13 +1,28 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
 #include "AccessControlSystem.h"
+#include "credentials.h"
 
 AccessControlSystem accessControlSystem;
 
 void setup() {
     Serial.begin(115200);
+    delay(1000);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected!");
+    Serial.println(WiFi.localIP());
+    
     accessControlSystem.setup();
-}
+    }
 
 void loop() {
     accessControlSystem.loop();
